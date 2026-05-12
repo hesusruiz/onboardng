667680c10772c7e0d3c42b17849d8911fc758c4b
Jesus Ruiz <hesus.ruiz@gmail.com>
2026-05-12 19:16:19 +0000

    fix serialNumber in Mandator

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -487,13 +487,13 @@ func performIssuance(ctx context.Context, token string, reg *db.RegistrationReco
 				Country:                requestData.Country,
 				CommonName:             requestData.FirstName + " " + requestData.LastName,
 				EmailAddress:           requestData.Email,
+				SerialNumber:           "1234567890",
 			},
 			Mandatee: credissuance.Mandatee{
-				FirstName:    requestData.FirstName,
-				LastName:     requestData.LastName,
-				Nationality:  requestData.Country,
-				Email:        requestData.Email,
-				SerialNumber: "1234567890",
+				FirstName:   requestData.FirstName,
+				LastName:    requestData.LastName,
+				Nationality: requestData.Country,
+				Email:       requestData.Email,
 			},
 			Power: []credissuance.Power{
 				{
